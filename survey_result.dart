f1291e139c1c32552d2742829e32c6c4b55a85b6
Weijen1006 <41354172+Weijen1006@users.noreply.github.com>
2019-12-17 21:21:06 +0800

    ok

--- survey_result.dart
+++ survey_result.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 
+
 class survey_Result extends StatefulWidget {
   int result;
+  String field;
   String description;
+  AssetImage image;
 
   survey_Result(int result){
     this.result = result;
-    if (this.result <= 17)
-      description = "Scientist";
-    else if (this.result >= 18 && this.result <= 26)
-      description = "Social Sciences Expert";
-    else if (this.result >= 27 && this.result <= 35)
-      description = "Humanitarian";
-    else if (this.result >= 36 && this.result <= 44)
-      description = "Artist";
-    else
-      description = "Sportsman";
-
+    if (this.result <= 17) {
+      field = "Scientist";
+      description = "• Chemistry\n• Physics\n• Biology";
+      image = new AssetImage('assets/images/science.jpg');
+    }else if (this.result >= 18 && this.result <= 26) {
+      field = "Social Sciences Expert";
+      description = "• Economics \n• History\n• Law\n• Lingustics\n• Sociology";
+      image = new AssetImage('assets/images/social_science.jpg');
+    }else if (this.result >= 27 && this.result <= 35) {
+      field = "Humanitarian";
+      description = "• Public Health in Humanitarian\n  Crises\n• Introduction to Humanitarian\n  Aid\n• Global Health and\n  Humanitarianism";
+      image = new AssetImage('assets/images/humanitarian.jpg');
+    }else if (this.result >= 36 && this.result <= 44) {
+      field = "Artist";
+      description = "• Animation\n• Art & Design\n• Art Theory\n• Illustration\n• Visual Arts\n• Art Studies";
+      image = new AssetImage('assets/images/artist.jpg');
+    }else {
+      field = "Sportsperson";
+      description = "• Formative Soccer Expert\n• Tennis Coach Internship\n• Course in Sports Agent\n• Expert Course in Sport\n  Social Media Manager";
+      image = new AssetImage('assets/images/sports.jpg');
+    }
   }
 
   @override
@@ -60,34 +73,69 @@ class _survey_ResultState extends State<survey_Result> {
     return WillPopScope(
         onWillPop: alertBack2,
         child: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                alertBack2();
-              },
-            ),
-            title: Text("Result"),
-          ),
+        appBar: AppBar(
+        leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+    onPressed: () {
+          alertBack2();
+    },
+    ),
+    title: Text("Result"),
+    ),
           body: Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Your total score is ${widget.result}",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),),
-                  Text("You are suitble to be a ${widget.description}",
-                      style: TextStyle(
-                        fontSize: 20,
-                      )),
-                ],
-              ),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+            Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                    image: widget.image,
+                  ),
+                ),
             ),
-
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("You are suitable to be a/an :\n",
+                    style: TextStyle(
+                      fontSize: 22,
+                    )),
+                    Text(widget.field,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text("Recommended field of study :",
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),),
+                      Text(widget.description,
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        )
+    )
     );
   }
 }
